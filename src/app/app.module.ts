30ec72a47229444cfbfd478e6f74f7463075c403
jhonnyperezm <j_perez5@unisimon.edu.co>
2019-08-05 10:52:44 -0500

    3 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { HttpClientModule} from '@angular/common/http';
+import { app_routing } from './router';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { GraficaComponent } from './grafica/grafica.component';
+import { CambioComponent } from './cambio/cambio.component';
+import { AppService } from './app.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule,    HighchartsChartModule,
+    HttpClientModule,
+    RouterModule,
+    app_routing ],
+  declarations: [ AppComponent, HelloComponent, GraficaComponent, CambioComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [AppService]
 })
 export class AppModule { }
